54b1a8205e7854d974df711e352bc81b1e13344f
ryder34 <aarvitech24@gmail.com>
2026-03-19 10:18:00 +0530

    final server ip added

--- DoctorNurseApp/utils/config.ts
+++ DoctorNurseApp/utils/config.ts
@@ -2,8 +2,8 @@
 // Declare __DEV__ for TypeScript
 declare const __DEV__: boolean;
 //wellness ip :'10.200.205.182' around range
-const SERVER_IP = '10.200.204.165'; // <-- Change this to your server's IP address
-const SERVER_PORT = '3000';
+const SERVER_IP = '10.195.250.67'; // <-- Change this to your server's IP address
+const SERVER_PORT = '43212';
 // ========================================
 
 export const CONFIG = {
